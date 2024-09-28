e138240d72a0ebaf11f965be2077f59b7accab80
LoganSwinson <97139851+LoganSwinson@users.noreply.github.com>
2024-09-28 15:18:12 -0700

    Initial project creation

---
+++ src/main/java/my/gcu/worse_amazon/WorseAmazonApplication.java
@@ -0,0 +1,13 @@
+package my.gcu.worse_amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorseAmazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorseAmazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/gcu/worse_amazon/WorseAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package my.gcu.worse_amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorseAmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
