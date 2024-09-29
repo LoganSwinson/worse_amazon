141aa480baf9cbcc2c7fba6949bf0b515e2da5ce
Cisco <ohdankiee@gmail.com>
2024-09-29 01:15:34 -0700

    Completed Code!

--- src/main/java/my/gcu/WorseAmazonApplication.java
+++ src/main/java/my/gcu/WorseAmazonApplication.java
@@ -1,4 +1,4 @@
-package my.gcu.worse_amazon;
+package my.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/my/gcu/controllers/RegisterController.java
+++ src/main/java/my/gcu/controllers/RegisterController.java
@@ -32,7 +32,8 @@ public class RegisterController
         }
 
         System.out.println(String.format("Form with a Username of %s and Password of %s", userModel.getUsername(), userModel.getPassword()));
-        return "login";
-    }
 
+        // Redirect to login after successful registration
+        return "redirect:/login";
+    }
 }
