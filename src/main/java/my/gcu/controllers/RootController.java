9abadfa9a0e862df9b8e0a5812c23bc60c347d1a
LoganSwinson <97139851+LoganSwinson@users.noreply.github.com>
2024-09-29 17:17:25 -0700

    Added redirect from root page to home page

---
+++ src/main/java/my/gcu/controllers/RootController.java
@@ -0,0 +1,16 @@
+package my.gcu.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/")
+    public String root()
+    {
+        // When the user goes to the "root" page, redirect to the "home" page
+        return "redirect:/home";
+    }
+}
