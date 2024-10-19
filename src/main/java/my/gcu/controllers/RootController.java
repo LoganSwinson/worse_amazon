3d08accc6159059fcfcac0bb09a9ab6ba1c07302
Cisco <ohdankiee@gmail.com>
2024-10-19 16:25:29 -0700

    yes

--- src/main/java/my/gcu/controllers/RootController.java
+++ src/main/java/my/gcu/controllers/RootController.java
@@ -1,7 +1,6 @@
 package my.gcu.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
